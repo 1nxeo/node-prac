cfdb437654c00a4c152a3dcfff3c3ab2f01f38c1
Inseo Yang <y.1nseooo@gmail.com>
2023-05-13 09:38:34 +0900

    미들웨어 설정하기

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ app.get("/", (req, res) => {
 // localhost:3000/api -> goodsRouter
 app.use("/api", [goodsRouter]);
 
+// app.use는 app.listen 위에 있어야 합니당.
+
+// request log middleware
+app.use((req, res, next) => {
+  console.log("Request URL:", req.originalUrl, " - ", new Date());
+  next();
+});
+
+// app.use(Middleware) => 모든 요청에서 미들웨어 실행
+// app.use('/api', Middleware) => /api로 시작하는 요청에서 미들웨어 실행
+// app.post('/api', Middleware) => /api로 시작하는 POST요청에서 미들웨어 실행
+
 app.listen(port, () => {
   console.log(port, "포트로 서버가 열렸어요!");
 });
